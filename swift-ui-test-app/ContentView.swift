f83477b00e18392c26b9342a59b0da58a16ecdfd
themaxermister <subscribingalerts@gmail.com>
2022-12-31 02:01:53 +0800

    INIT

--- swift-ui-test-app/ContentView.swift
+++ swift-ui-test-app/ContentView.swift
@@ -5,17 +5,67 @@
 //  Created by Jeremy Chua on 31/12/22.
 //
 
+
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+class ViewModel:ObservableObject {
+    @Published var image: Image?
+    func fetchNewImage() {
+        guard let url = URL(string: "https://picsum.photos/200") else {
+            return
         }
-        .padding()
+        
+        let task = URLSession.shared.dataTask(with: url) {
+            data, _, _ in
+                guard let data = data
+            else { return }
+            DispatchQueue.main.async {
+                guard let uiImage = UIImage(data: data) else { return  }
+                self.image = Image(uiImage: uiImage)
+            }
+        }
+        task.resume()
+    }
+}
+
+struct ContentView: View {
+    @StateObject var viewModel = ViewModel()
+    var body: some View {
+        NavigationView {
+            VStack{
+                Spacer()
+                
+                if let image = viewModel.image {
+                    ZStack {
+                        image
+                           .resizable()
+                           .foregroundColor(.mint)
+                           .frame(width: 200, height: 200)
+                    }
+                    .frame(width: UIScreen.main.bounds.width / 1.2, height: UIScreen.main.bounds.width / 1.2)
+                    .background(.mint)
+                    .cornerRadius(10)
+                } else {
+                    Image(systemName: "photo")
+                       .resizable()
+                       .foregroundColor(.mint)
+                       .frame(width: 300, height: 300)
+                }
+                Spacer()
+                
+                Button(action: {
+                    viewModel.fetchNewImage()
+                }, label:{
+                    Text("Next Picture")
+                        .frame(width: 250, height: 55)
+                        .foregroundColor(.white)
+                        .background(.teal)
+                        .cornerRadius(10)
+                        .padding()
+                })
+            }
+            .navigationTitle("Random Photo")
+        }
     }
 }
 

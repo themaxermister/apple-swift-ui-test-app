548af8a51aae5d0c6d51b34db915d152033f4e4a
themaxermister <subscribingalerts@gmail.com>
2022-12-31 01:40:59 +0800

    Initial Commit

---
+++ swift-ui-test-app/swift_ui_test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_test_appApp.swift
+//  swift-ui-test-app
+//
+//  Created by Jeremy Chua on 31/12/22.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
